00413fb76b2c52acefe367a0c28aea87b1669ccc
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-24 07:00:47 -0800

    commit tree 4864b1c5ba04bb6d40ec594379ae3a74734c99e1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,30 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+   # Add this line to set the Content-Type header for all responses
+   set :default_content_type, 'application/json'
+
+  get '/games' do
+  #   # get all the games from the database
+  #   games = Game.all
+  #   # return a JSON response with an array of all the game data
+  #   games.to_json
+  # end
+
+  #   games = Game.all.order(:title)
+  #   games.to_json
+  # end
+
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+
   end
 
+   # use the :id syntax to create a dynamic route
+   get '/games/:id' do
+        # look up the game in the database using its ID
+        game = Game.find(params[:id])
+        # send a JSON-formatted response of the game data
+        # include associated reviews in the JSON response
+        game.to_json(include: { reviews: { include: :user } })
+        end
 end
